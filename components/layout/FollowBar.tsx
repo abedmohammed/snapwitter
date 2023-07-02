93f5a0ee7882e306294bf079dafe1db09fbf43e5
abedm <abedmohammed353@gmail.com>
2023-07-02 08:50:05 -0400

    Sticky nav and load more button

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -10,7 +10,7 @@ const FollowBar = () => {
 
   return (
     <div className="px-6 py-4 hidden lg:block">
-      <div className="bg-neutral-800 rounded-xl p-4">
+      <div className="bg-neutral-800 rounded-xl p-4 sticky top-4">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
           {users.slice(0, 10).map((user: Record<string, any>) => (
--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,5 +1,6 @@
 import usePosts from "@/hooks/usePosts";
 import PostItem from "./PostItem";
+import Button from "../Button";
 
 interface PostFeedProps {
   userId?: string;
@@ -13,6 +14,9 @@ const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
       {posts.map((post: Record<string, any>) => (
         <PostItem userId={userId} key={post.id} data={post} />
       ))}
+      <div className="w-full flex flex-col items-center pt-8 pb-4">
+        <Button label="Load more" outline onClick={() => {}} />
+      </div>
     </>
   );
 };
